083b7ac9d5ed0ffe4544ea2c12bca91e9698fd8b
edwincusin1 <edwin32@csn.com>
2021-03-24 16:58:53 -0500

    iniciando git

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css">
+        <link rel="stylesheet" href="./css/estilos1.css">
+    <title>Acceso</title>
+</head>
+
+<body id="body_log">
+    <form action="" method="post" id="form_log" class="animate__animated animate__backInLeft">
+        <div >
+            <!-- // comentario div imagen url desde css -->
+            <img src="./img/LogoEsfoguar.png" alt="" id="img_log">
+            <a href="https://www.esfoguar.com/esfoguar/index.php">www.esfoguar.com</a>
+
+        </div>
+
+        <div id="separador_log" >
+            <h1 class="animate__animated animate__backInLeft">Iniciar sesión</h1>
+            <h2>Siccoes</h2>
+            <p><img src="./img/icono_login.png" alt="" id="img_login_icono"></p>            
+            <p>&#128102 <input type="text" placeholder="&#128100; Ingrese cédula" name="usuario"  maxlength="10">  </p>            
+            <p>&#128274 <input type="password" placeholder="&#128274; Ingrese contraseña" name="contraseña"  maxlength="20"> </p>
+            <p>
+            <input type="submit" value="&#128272; Entrar" >      
+            <input type="submit" value="&#128273 ¿Olvidó su contraseña?" >
+        </p>
+           
+
+        </div>
+    </form>
+</body>
+
+</html>
