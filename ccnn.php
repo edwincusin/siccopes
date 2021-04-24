21e9050535c5e6185f001fab67c806b71d8a0b72
edwinecuador <edwin@ecuador.com>
2021-04-24 03:18:40 -0500

    probando conexion dbconn

--- ccnn.php
+++ ccnn.php
@@ -1,11 +1,39 @@
 <?php
-//conectamos con el servidor
-$con= pg_connect("host='localhost' dbname=siccopes port=5432 user=postgres password=admin1996") ;
-//verificamos la conexion
-if($con){
+//conectamos con el servidor de la bbd
+$dbconn= pg_connect("host='localhost' dbname=siccopes port=5432 user=postgres password=admin1996") ;
+//verificamos la conexion establecida con un mensaje
+if($dbconn){
  echo "Conectado";
-}else { echo "No se pudo conectar con el servidor";
+
+}else { echo "No se pudo conectar con el servidor, error";
 }
-//$consulta= "INSERT INTO aa_hola_mundo VALUES (3, 'tres',04)";
-//$result = pg_query($consulta) or die('La consulta fallo: ' . pg_last_error());
+
+/*
+// Realizando una consulta SQL
+$query = 'SELECT * FROM public.sexo';
+$result = pg_query($query) or die('La consulta fallo: ' .pg_last_error());
+
+// Imprimiendo los resultados en HTML
+echo "<table>\n";
+while ($line = pg_fetch_array($result, NULL, PGSQL_ASSOC)) {
+    echo "\t<tr>\n";
+    foreach ($line as $col_value) {
+        echo "\t\t<td>$col_value</td>\n";
+    }
+    echo "\t</tr>\n";
+}
+echo "</table>\n";
+
+ //Liberando el conjunto de resultados
+pg_free_result($result);
+
+// Cerrando la conexión
+pg_close($dbconn);
+
+*/
+
+ 
 ?>
+
+
+
